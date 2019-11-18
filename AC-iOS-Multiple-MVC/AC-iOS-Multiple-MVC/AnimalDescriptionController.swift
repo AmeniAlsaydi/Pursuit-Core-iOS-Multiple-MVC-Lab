2a47f03eb7bffe5971b13b3bb9fc1848625aad5f
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-18 17:23:01 -0500

    cells appropriatly populate

---
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/AnimalDescriptionController.swift
@@ -0,0 +1,20 @@
+//
+//  AnimalDescriptionController.swift
+//  AC-iOS-Multiple-MVC
+//
+//  Created by Amy Alsaydi on 11/18/19.
+//  Copyright © 2019 AC-iOS. All rights reserved.
+//
+
+import UIKit
+
+class AnimalDescriptionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
+++ AC-iOS-Multiple-MVC/AC-iOS-Multiple-MVC/ViewController.swift
@@ -22,7 +22,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        animals = ZooAnimal.zooAnimals
+        animalTableView.dataSource = self
+        animalTableView.delegate = self
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -30,5 +35,25 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath) as? AnimalCell else { fatalError("could not dequeue a animalCell") }
+        
+        let animal = animals[indexPath.row]
+        
+        cell.configureCell(for: animal)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
 
